6beb6d57d473388e95d6858a4145de2f2aafac81
dev-haze <62634831+dev-haze@users.noreply.github.com>
2022-12-08 14:42:52 +0900

    test

---
+++ champ/champ_frame.js
@@ -0,0 +1,27 @@
+class champ_frame{
+    constructor(){
+        this.hp = 1;
+        this.hpgen=1;
+        this.mp=1;
+        this.mpgen=1;
+
+        this.items= new Array();
+    }
+
+    Q(){
+
+    }
+    W(){
+
+    }
+    E(){
+
+    }
+    R(){
+
+    }
+}
+
+class item_frame{
+
+}
--- test/test.js
+++ test/test.js
@@ -1,1 +1,20 @@
-alert("test");
+class parent{
+    constructor(a){
+        this.a = a;
+    }
+
+    show(){
+        alert(this.a);
+    }
+}
+
+
+class children extends parent{
+    constructor(b){
+        this.b=b;
+    }
+}
+
+
+let cd = new children(1,2);
+cd.show();
